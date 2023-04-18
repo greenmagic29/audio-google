ecbb901226bb91ae4f93e28eadd4c35859e66ccc
thomas <tyic2834@gmail.com>
2023-04-19 00:26:27 +0800

    guess with time

--- guess_file.js
+++ guess_file.js
@@ -1,11 +1,29 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const fs = require('fs');
-async function lsWithGrep() {
+const dayjs = require('dayjs')
+var customParseFormat = require('dayjs/plugin/customParseFormat')
+dayjs.extend(customParseFormat)
+
+async function lsWithGrep(time) {
   try {
-      const { stdout, stderr } = await exec(`find . -newermt '2023-04-17 20:47' ! -newermt '2023-04-18 20:48' -ls`);
+      //const timeInMss = new Date().getTime();
+      const searchTime = time.format('YYYY-MM-DD HH:mm');
+      const searchTimePlusOneMin = time.add(1, 'm').format('YYYY-MM-DD HH:mm');
+      const { stdout, stderr } = await exec(`find /c/Users/wyp60/Documents/audio-google/WhatsApp_Voice_Notes -newermt '${searchTime}' ! -newermt '${searchTimePlusOneMin}' -ls`);
       console.log('stdout:', stdout);
-      console.log('stderr:', stderr);
+      //console.log('stderr:', stderr);
+      const keyword = new RegExp('.* (/c/Users.*)\n', 'gi');
+      const mm = stdout.matchAll(keyword);
+      let saveFilePathString = "";
+      for (const match of mm) {
+        saveFilePathString += match[1];
+        saveFilePathString += "\n";
+        //console.log(match[1]);
+        //console.log(match.index)
+      }
+      return saveFilePathString;
+      //fs.writeFileSync(`./saveFilePath-${timeInMss}.txt`, saveFilePathString, {encoding: 'utf-8'});
   }catch (err) {
      console.error(err);
   };
@@ -15,9 +33,23 @@ async function lsWithGrep() {
 async function main() {
   try {
     let raw_text = fs.readFileSync('./chat_old.txt', {encoding: 'utf8'});
-    const keyword = new RegExp('.* <Media omitted>\n', 'g');
+    const keyword = new RegExp('(.*) - .* <Media omitted>\n', 'gi');
+    const mm = raw_text.matchAll(keyword);
+    const changeDates = [];
+    //console.log("🚀 ~ file: guess_file.js:20 ~ main ~ mm:", mm)
+    for (const match of mm) {
+      //19/11/2022 14:34 
+      const dateObj = dayjs(match[1].trim(), 'D/M/YYYY HH:mm')
+      //.format('YYYY-MM-DD HH:mm');
+      const guessFiles = await lsWithGrep(dateObj);
+      //console.log(`${match[1]}:${result}`);
+      //console.log(match.index)
+    }
+
+    
     
   } catch (error) {
     
   }
 }
+main()
